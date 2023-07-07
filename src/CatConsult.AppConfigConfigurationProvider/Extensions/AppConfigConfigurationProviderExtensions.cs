e3ebed641161b22351a6340bc80aa73dafb09317
Derek McKinnon <mckinnon.derek@gmail.com>
2023-07-07 09:43:05 -0400

    Deprecate `CatConsult.AppConfigConfigurationProvider.Extensions` namespace

--- src/CatConsult.AppConfigConfigurationProvider/Extensions/AppConfigConfigurationProviderExtensions.cs
+++ src/CatConsult.AppConfigConfigurationProvider/Extensions/AppConfigConfigurationProviderExtensions.cs
@@ -1,37 +1,10 @@
-using CatConsult.AppConfigConfigurationProvider.Utilities;
-
 using Microsoft.Extensions.Configuration;
 
 namespace CatConsult.AppConfigConfigurationProvider.Extensions;
 
 public static class AppConfigConfigurationProviderExtensions
 {
-    public static IConfigurationBuilder AddAppConfig(
-        this IConfigurationBuilder builder,
-        string sectionName = "AppConfig"
-    )
-    {
-        var options = builder.Build()
-            .GetSection(sectionName)
-            .Get<AppConfigOptions>();
-
-        if (options is null)
-        {
-            return builder;
-        }
-
-        var profiles = options.Profiles.Select(AppConfigProfileParser.Parse);
-
-        foreach (var profile in profiles)
-        {
-            builder.AddAppConfig(
-                profile.ApplicationId,
-                profile.EnvironmentId,
-                profile.ProfileId,
-                TimeSpan.FromSeconds(profile.ReloadAfter ?? options.Defaults.ReloadAfter)
-            );
-        }
-
-        return builder;
-    }
+    [Obsolete("Use CatConsult.AppConfigConfigurationProvider instead - this will be removed in a future version")]
+    public static IConfigurationBuilder AddAppConfig(this IConfigurationBuilder builder, string sectionName = "AppConfig") =>
+        AppConfigConfigurationProvider.AppConfigConfigurationProviderExtensions.AddAppConfig(builder, sectionName);
 }
