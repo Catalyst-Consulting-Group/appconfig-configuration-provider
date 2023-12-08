94e7d83695a82f372f15477ba77bf489e2f6d7bd
Derek McKinnon <mckinnon.derek@gmail.com>
2023-12-08 11:44:27 -0500

    Add sample application for testing

---
+++ samples/AppConfigTesting/Program.cs
@@ -0,0 +1,46 @@
+using CatConsult.AppConfigConfigurationProvider;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddAppConfig();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.AddOptions<YamlTest>()
+    .BindConfiguration("YamlTest");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
+
+public class YamlTest
+{
+    public bool Test { get; set; }
+
+    public Person Person { get; set; } = new();
+}
+
+public class Person
+{
+    public string Name { get; set; } = string.Empty;
+
+    public int Age { get; set; }
+}
