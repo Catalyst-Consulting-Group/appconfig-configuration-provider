8e2865f83433a91fbea002417f1c2e95532f234f
Derek McKinnon <mckinnon.derek@gmail.com>
2024-01-02 20:12:15 -0500

    Add `FeatureFlags` section to the configuration

--- src/CatConsult.AppConfigConfigurationProvider/AppConfigOptions.cs
+++ src/CatConsult.AppConfigConfigurationProvider/AppConfigOptions.cs
@@ -4,10 +4,19 @@ public class AppConfigOptions
 {
     public List<string> Profiles { get; set; } = new();
 
+    public List<string> FeatureFlags { get; set; } = new();
+
     public Defaults Defaults { get; set; } = new();
 }
 
 public class Defaults
 {
     public int ReloadAfter { get; set; } = 60;
+
+    public FeatureFlagDefaults FeatureFlags { get; set; } = new();
+}
+
+public class FeatureFlagDefaults
+{
+    public string SectionName { get; set; } = "FeatureManagement";
 }
